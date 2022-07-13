4ae1bcbda8f639b10cdb214065ba48fdb2134cb4
Juli Romero <juliiromero2@gmail.com>
2022-07-13 20:36:13 -0300

    Algunos cambios en Razon, todavia faltan algunos

--- SdC/Clientes.py
+++ SdC/Clientes.py
@@ -1,6 +1,13 @@
+from datetime import datetime
 from Direccion import Direccion
+from SdC.Razon import RazonAltaChequera, RazonAltaTarjetaCredito, RazonRetiroEfectivo
 
 class Clientes:
+    transacciones = []
+    saldoEnCuenta = 0
+    cantTarjetasCredito = 0
+    cantChequeras = 0
+    cupoDiarioRestante = 0
     def __init__(self, datos):
         self.nombre = datos['nombre']
         self.apellido = datos['apellido']
@@ -21,8 +28,52 @@ class Clientes:
     def puede_comprar_dolar():
         return None
     
+    def puede_retirar(monto):
+        return True
+
+    def retirar_efectivo(self,monto):
+        self.saldoEnCuenta -= monto
+    
+    def nueva_chequera(self):
+        self.cantChequeras += 1
+
     def cantTarjetasCredito(datos):
         return datos['transacciones']['totalTarjetasDeCreditoActualmente']
 
     def cantChequeras(datos):
         return datos['transacciones']['totalChequerasActualmente']
+    
+    def cantidad_transacciones(self):
+        return len(self.transacciones)
+
+    def RETIRO_EFECTIVO_CAJERO_AUTOMATICO(self,monto,cuentaNumero):
+        if (self.puede_retirar(monto)):
+            retiroEfvo = RazonRetiroEfectivo("APROBADO", cuentaNumero, self.cupoDiarioRestante , monto,datetime.now,
+            self.cantidad_transacciones() +1, self.saldoEnCuenta, self.cantTarjetasCredito(), self.cantChequeras() )
+            retiroEfvo.resolver(self, monto)
+        else:
+            retiroEfvo= RazonRetiroEfectivo("RECHAZADO", cuentaNumero, self.cupoDiarioRestante , monto,datetime.now,
+            self.cantidad_transacciones() +1, self.saldoEnCuenta, self.cantTarjetasCredito(), self.cantChequeras() )
+
+        self.transacciones.append(retiroEfvo)
+
+    def ALTA_TARJETA_CREDITO(self, monto, cuentaNumero):
+        if (self.puede_crear_tarjeta_credito()):
+            nuevaTarjeta = RazonAltaTarjetaCredito("APROBADO", cuentaNumero, self.cupoDiarioRestante , monto,datetime.now,
+            self.cantidad_transacciones() +1, self.saldoEnCuenta, self.cantTarjetasCredito(), self.cantChequeras() )
+            nuevaTarjeta.resolver(self,monto)
+        else:
+            nuevaTarjeta = RazonAltaTarjetaCredito("RECHAZADO", cuentaNumero, self.cupoDiarioRestante , monto,datetime.now,
+            self.cantidad_transacciones() +1, self.saldoEnCuenta, self.cantTarjetasCredito(), self.cantChequeras() )
+    
+        self.transacciones.append(nuevaTarjeta)
+
+    def ALTA_CHEQUERA(self,monto,cuentaNumero):
+        if (self.puede_crear_tarjeta_credito()):
+            altaChequera = RazonAltaChequera("APROBADO", cuentaNumero, self.cupoDiarioRestante , monto,datetime.now,
+            self.cantidad_transacciones() +1, self.saldoEnCuenta, self.cantTarjetasCredito(), self.cantChequeras() )
+            altaChequera.resolver(self,monto)
+        else:
+            altaChequera = RazonAltaTarjetaCredito("RECHAZADO", cuentaNumero, self.cupoDiarioRestante , monto,datetime.now,
+            self.cantidad_transacciones() +1, self.saldoEnCuenta, self.cantTarjetasCredito(), self.cantChequeras() )
+        self.transacciones.append(altaChequera)
--- SdC/Razon.py
+++ SdC/Razon.py
@@ -1,30 +1,70 @@
+
 class Razon:
-    def __init__(self, type):
-        self.type = type
+    def __init__(self,estado,tipo,cuentaNumero,
+    CupoDiarioRestante,monto,fecha, numero,saldoEnCuenta,
+    totalTarjetasDeCreditoActualmente,totalChequerasActualmente):
+        self.estado = estado
+        self.tipo = tipo
+        self.cuentaNumero = cuentaNumero
+        self.CupoDiarioRestante = CupoDiarioRestante
+        self.monto = monto
+        self.fecha = fecha
+        self.numero = numero
+        self.saldoEnCuenta = saldoEnCuenta
+        self.totalTarjetasDeCreditoActualmente = totalTarjetasDeCreditoActualmente
+        self.totalChequerasActualmente = totalChequerasActualmente
+
     
-    def resolver(self,cliente,evento):
+    def resolver(self,cliente,monto):
          pass
 
 class RazonAltaChequera(Razon):
-    def __init__(self, type):
-        super().__init__(type)
+    def __init__(self, estado, cuentaNumero, CupoDiarioRestante, monto,
+    fecha, numero, saldoEnCuenta, totalTarjetasDeCreditoActualmente, totalChequerasActualmente):
+        super().__init__(estado, cuentaNumero, CupoDiarioRestante, monto, fecha, numero,
+        saldoEnCuenta, totalTarjetasDeCreditoActualmente, totalChequerasActualmente)
+        self.tipo = "ALTA_CHEQUERA"
+    
+    def resolver(self, cliente, monto):
+        cliente.nueva_chequera()
     
 class RazonAltaTarjetaCredito(Razon):
-    def __init__(self, type):
-        super().__init__(type)
+    def __init__(self, estado, cuentaNumero, CupoDiarioRestante, monto,
+    fecha, numero, saldoEnCuenta, totalTarjetasDeCreditoActualmente, totalChequerasActualmente):
+        super().__init__(estado, cuentaNumero, CupoDiarioRestante, monto, fecha, numero,
+        saldoEnCuenta, totalTarjetasDeCreditoActualmente, totalChequerasActualmente)
+        self.tipo = "ALTA_TARJETA_CREDITO"
+    
+    def resolver(self, cliente, monto):
+        cliente.cantTarjetasCredito += 1
 
 class RazonCompraDolar(Razon):
-    def __init__(self, type):
-        super().__init__(type)
+    def __init__(self, estado, cuentaNumero, CupoDiarioRestante, monto,
+    fecha, numero, saldoEnCuenta, totalTarjetasDeCreditoActualmente, totalChequerasActualmente):
+        super().__init__(estado, cuentaNumero, CupoDiarioRestante, monto, fecha, numero,
+        saldoEnCuenta, totalTarjetasDeCreditoActualmente, totalChequerasActualmente)
+        self.tipo = "COMPRAR_DOLAR"
 
 class RazonRetiroEfectivo(Razon):
-    def __init__(self, type):
-        super().__init__(type)
-
+    def __init__(self, estado, cuentaNumero, CupoDiarioRestante, monto,
+    fecha, numero, saldoEnCuenta, totalTarjetasDeCreditoActualmente, totalChequerasActualmente):
+        super().__init__(estado, cuentaNumero, CupoDiarioRestante, monto, fecha, numero,
+        saldoEnCuenta, totalTarjetasDeCreditoActualmente, totalChequerasActualmente)
+        self.tipo = "RETIRO_EFECTIVO_CAJERO_AUTOMATICO"
+    
+    def resolver(self, cliente, monto):
+        cliente.retirar_efectivo(monto)
+        
 class RazonTransferenciaEnviada(Razon):
-    def __init__(self, type):
-        super().__init__(type)
+    def __init__(self, estado, cuentaNumero, CupoDiarioRestante, monto,
+    fecha, numero, saldoEnCuenta, totalTarjetasDeCreditoActualmente, totalChequerasActualmente):
+        super().__init__(estado, cuentaNumero, CupoDiarioRestante, monto, fecha, numero,
+        saldoEnCuenta, totalTarjetasDeCreditoActualmente, totalChequerasActualmente)
+        self.tipo = "TRANSFERENCIA_ENVIADA"
 
 class RazonTransferenciaRecibida(Razon):
-    def __init__(self, type):
-        super().__init__(type)
+    def __init__(self, estado, cuentaNumero, CupoDiarioRestante, monto,
+    fecha, numero, saldoEnCuenta, totalTarjetasDeCreditoActualmente, totalChequerasActualmente):
+        super().__init__(estado, cuentaNumero, CupoDiarioRestante, monto, fecha, numero,
+        saldoEnCuenta, totalTarjetasDeCreditoActualmente, totalChequerasActualmente)
+        self.tipo = "TRANSFERENCIA_RECIBIDA"
