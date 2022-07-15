2dda6e44fc6cd9a151ebab9c3724cea6ccbd8dfe
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-07-15 16:05:21 -0300

    Creado template HTML

---
+++ SdC/content.py
@@ -0,0 +1,60 @@
+content = f'<!DOCTYPE html> \
+<html lang="en"> \
+<head> \
+    <meta charset="UTF-8"> \
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> \
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> \
+    <title>Segmentacion de clientes</title> \
+    <link rel="stylesheet" href="./assets/styles/reset.css"> \
+    <link rel="stylesheet" href="./assets/styles/styles.css"> \
+    <link rel="preconnect" href="https://fonts.googleapis.com"> \
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=""> \
+    <link href="https://fonts.googleapis.com/css2?family=Gugi&amp;family=Lato:wght@300&amp;display=swap" rel="stylesheet"> \
+</head> \
+<body> \
+    <section class="client classic"> \
+        <h2 class="classic__title title">Classic</h2> \
+        <div class="classic__data data"> \
+            <p class="data-item"><span id="changeFormat">Nombre:</span><span class="name">(Dato)</span></p> \
+            <p class="data-item"><span id="changeFormat">Numero:</span><span class="number">(Dato)</span></p> \
+            <p class="data-item"><span id="changeFormat">DNI:</span><span class="dni">(Dato)</span></p> \
+            <p class="data-item"><span id="changeFormat">Direccion:</span><span class="address">(Dato)</span></p> \
+        </div> \
+        <div class="transactions"> \
+            <h3 class="subtitle title">Transacciones</h3> \
+            <ul class="transactions__list"> \
+            </ul> \
+        </div> \
+    </section> \
+    <section class="client gold"> \
+        <h2 class="gold__title title">Gold</h2> \
+        <div class="gold__data data"> \
+            <p class="data-item"><span id="changeFormat">Nombre:</span><span class="name">(Dato)</span></p> \
+            <p class="data-item"><span id="changeFormat">Numero:</span><span class="number">(Dato)</span></p> \
+            <p class="data-item"><span id="changeFormat">DNI:</span><span class="dni">(Dato)</span></p> \
+            <p class="data-item"><span id="changeFormat">Direccion:</span><span class="address">(Dato)</span></p> \
+        </div> \
+        <div class="transactions"> \
+            <h3 class="subtitle title">Transacciones</h3> \
+            <ul class="transactions__list"> \
+            </ul> \
+        </div> \
+    </section> \
+    <section class="client black"> \
+        <h2 class="black__title title">Black</h2> \
+        <div class="black__data data"> \
+            <p class="data-item"><span id="changeFormat">Nombre:</span><span class="name">(Dato)</span></p> \
+            <p class="data-item"><span id="changeFormat">Numero:</span><span class="number">(Dato)</span></p> \
+            <p class="data-item"><span id="changeFormat">DNI:</span><span class="dni">(Dato)</span></p> \
+            <p class="data-item"><span id="changeFormat">Direccion:</span><span class="address">(Dato)</span></p> \
+        </div> \
+        <div class="transactions"> \
+            <h3 class="subtitle title">Transacciones</h3> \
+            <ul class="transactions__list"> \
+            </ul> \
+        </div> \
+    </section> \
+    <script type="module" src="./assets/script.js"></script> \
+</body> \
+</html>  \
+'
