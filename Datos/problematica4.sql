2c5927fb2e8511836fbc5c818ab334d39f7701d8
Juli Romero <juliiromero2@gmail.com>
2022-07-29 19:57:05 -0300

    agrego 4.5

--- Datos/problematica4.sql
+++ Datos/problematica4.sql
@@ -48,5 +48,42 @@ LEFT JOIN (
 ON prestamos.customer_id = cliente.customer_id
 GROUP BY cliente.branch_id
 
+-- 4.5
+CREATE TABLE  IF NOT EXISTS auditoria_cuenta(
+							old_id INTEGER,
+							new_id INTEGER PRIMARY KEY,
+							old_balance INTEGER NOT NULL,
+							new_balance INTEGER NOT NULL,
+							old_iban TEXT NOT NULL,
+							new_iban TEXT NOT NULL,
+							old_type INTEGER NOT NULL,
+							new_type INTEGER NOT NULL,
+							user_action TEXT NOT NULL,
+							created_at TEXT NOT NULL	
+							);
+-- 4.5.1					
+CREATE TRIGGER IF NOT EXISTS post_actualizacion_cuentas 
+   AFTER UPDATE 
+   ON cuenta
+		WHEN old.balance <> new.balance 
+			OR old.iban <> new.iban
+				OR  old.tipo_cuenta <> new.tipo_cuenta
+BEGIN
+	INSERT INTO auditoria_cuenta(
+		old_id, new_id, old_balance, new_balance, old_iban, new_iban, old_type,
+		new_type, user_action, created_at )
+	VALUES (
+		old.account_id, new.account_id, old.balance, new.balance, old.iban, new.iban, old.tipo_cuenta,
+		new.tipo_cuenta, 'UPDATE', DATETIME('now')
+		);
+END;
+-- 4.5.2
+
+UPDATE cuenta
+SET balance = balance - 100
+WHERE account_id >= 10 AND account_id <= 14;
+-- verifico que se crearon los campos en la tabla nueva
+SELECT *
+FROM auditoria_cuenta
 
 
