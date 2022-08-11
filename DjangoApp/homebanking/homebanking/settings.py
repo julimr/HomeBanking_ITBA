cebdc3b5fe4c03a5ac3ad919c9d68e39bfd8b9cb
Juli Romero <juliiromero2@gmail.com>
2022-08-11 11:18:02 -0300

    cambio base de datos default por itbank.db

--- DjangoApp/homebanking/homebanking/settings.py
+++ DjangoApp/homebanking/homebanking/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'homebanking.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'itbank':{
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+    'default':{
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'itbank.db',
     }
