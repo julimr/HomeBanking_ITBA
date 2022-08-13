9aa0822d9f03031070d89b728319a18e7f9176a0
Juli Romero <juliiromero2@gmail.com>
2022-08-12 21:07:21 -0300

    cambios en clientes por modificacion de tabla

--- DjangoApp/homebanking/clientes/models.py
+++ DjangoApp/homebanking/clientes/models.py
@@ -8,6 +8,7 @@ class Cliente(models.Model):
     customer_dni = models.TextField(db_column='customer_DNI')  # Field name made lowercase.
     dob = models.TextField(blank=True, null=True)
     branch_id = models.IntegerField()
+    tipo_cliente = models.IntegerField()
 
     class Meta:
         managed = False
