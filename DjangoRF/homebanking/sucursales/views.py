fb0dc4f07e50b2e384f8543450102517140af339
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-08-21 22:33:18 -0300

    Cambio de return en view

--- DjangoRF/homebanking/sucursales/views.py
+++ DjangoRF/homebanking/sucursales/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from sucursales.models import Sucursal, Direccion
-from sucursales.serializers import SucursalSerializer, DireccionSerializer
+from sucursales.models import Direccion
+from sucursales.serializers import DireccionSerializer
+from rest_framework.renderers import TemplateHTMLRenderer, JSONRenderer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,10 +9,13 @@ from rest_framework import status
 
 # Create your views here.
 class SucursalesList(APIView):
+    renderer_classes = [JSONRenderer, TemplateHTMLRenderer]
+    template_name = "sucursales/index.html"
     def get(self, request):
         direccion = Direccion.objects.filter(branch__lte = 100)
         dir_serializer = DireccionSerializer(direccion, many=True)
-        print(dir_serializer.data)
-        return render(request, 'sucursales/index.html', {'direcciones' : dir_serializer.data})
+        #return render(request, 'sucursales/index.html', {'direcciones' : dir_serializer.data})
+        return Response({'direcciones' : dir_serializer.data})
+
 
 
